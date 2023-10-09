fc8a2f663bed6e3e3850029d7d06ef012073d77f
Anderson <andeedmond@gmail.com>
2023-10-09 11:13:31 -0700

    Added new projects and updated some projects with new code concepts

--- YummyCatering/src/YummyMoto2.java
+++ YummyCatering/src/YummyMoto2.java
@@ -2,10 +2,12 @@
 public class YummyMoto2 {
 
 	public static void main(String[] args) {
+		printCompanyMotto();
+
+	}
+	public static void printCompanyMotto() {
 		System.out.println("********************************************");
 		System.out.println("*Yummy makes the food that makes it a party*");
 		System.out.println("********************************************");
-
 	}
-
 }
